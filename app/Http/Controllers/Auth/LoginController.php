1ddf33291fff48569e4389827a369a8ab3edb2d3
Unknown <sxpcrazy@gmail.com>
2017-01-19 18:02:59 +0800

    Add account login implement

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,15 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+      return response()->json(['res' => 0]);
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+      return response()->json(['res' => -1, 'msg' => ['登录失败']]);
     }
 }
